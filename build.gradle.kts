91c2ffa23aff655ab3a0b6d7e382bef3e87ff346
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-12-27 10:18:31 +0330

    Use main sourceSet for solutions' place

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,7 @@ plugins {
     kotlin("jvm") version "1.7.21"
 }
 
-repositories {
-    mavenCentral()
-}
-
 tasks {
-    sourceSets {
-        main {
-            java.srcDirs("src")
-        }
-    }
-
     wrapper {
         gradleVersion = "7.6"
     }
