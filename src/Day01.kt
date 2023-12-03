6028b7bfd01940d5f4769c13e49f0b21cfab407d
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-12-03 17:43:51 +0330

    Add Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
+    val input = readInput("Day01")
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val isDigit = Char::isDigit
+            val first = it.first(isDigit)
+            val last = it.last(isDigit)
+            "$first$last".toInt()
+        }
     }
+    check(part1(readInput("Day01_part1_test")) == 142)
+    println(part1(input))
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digitWordsMap = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+        )
+        val allPossibleValues = digitWordsMap.flatMap { listOf(it.key, it.value.toString()) }
+
+        fun String.toDigit() = digitWordsMap[this] ?: toInt()
+
+        return input.sumOf {
+            val (_, first) = it.findAnyOf(allPossibleValues) ?: error("Impossible")
+            val (_, last) = it.findLastAnyOf(allPossibleValues) ?: error("Impossible")
+            first.toDigit() * 10 + last.toDigit()
+        }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
+    check(part2(readInput("Day01_part2_test")) == 281)
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -18,4 +18,4 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
-fun Any?.println() = println(this)
+fun <T> T.println(): T = also(::println)
