9c84d2227f320b142898f75148a583388c79cfa4
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-12-02 13:44:33 +0330

    Add Power Assert plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     kotlin("jvm") version "2.1.0"
+    kotlin("plugin.power-assert") version "2.1.0"
 }
 
 sourceSets {
@@ -12,4 +15,10 @@ tasks {
     wrapper {
         gradleVersion = "8.11.1"
     }
+}
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+powerAssert {
+    functions = listOf("kotlin.check")
+    includedSourceSets = listOf("main")
 }
