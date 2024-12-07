cc36bcfcd4a5a62eecfb9938339daa7618009ecd
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-12-07 15:21:20 +0330

    Generalize println function

--- src/Utils.kt
+++ src/Utils.kt
@@ -18,4 +18,4 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
-fun Any?.println() = also { println(this) }
+fun <T> T.println() = also { println(this) }
